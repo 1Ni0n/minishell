8f9ab1f7a7b507c015198c90001040ec2065d73e
Anteo GUILLOT <aguillot@e1r2p12.42.fr>
2018-10-02 19:23:25 +0200

    doing expansions

--- includes/manage_money.c
+++ includes/manage_money.c
@@ -34,7 +34,6 @@ int		find_noalnum(char *tmp)
 	off_t i;
 
 	i = 0;
-	printf("TMP: %s\n", tmp);
 	while (tmp[i])
 	{
 		if (ft_isalnum(tmp[i]) == 0)
@@ -48,22 +47,23 @@ int		modify_word(char **word, t_env_list *env_list, off_t i)
 {
 	char 	*tmpstart;
 	char 	*tmpend;
+	char	*middle;
 	char	*var;
 	off_t	j;
 
 	tmpend = NULL;
 	var = NULL;
 	tmpstart = ft_strsub(*word, 0, i);
-	printf("%s\n", tmpstart);
 	if ((j = find_noalnum(*word + i + 1)) != -1)
 	{
-		tmpend = ft_strdup((*word) + j);
-		printf("%s\n", tmpend);
-		if ((var = find_env_content(env_list, tmpend)) != NULL)
+		tmpend = ft_strdup((*word) + i + j + 1);
+		middle = ft_strsub((*word), i + 1, j);
+		if ((var = find_env_content(env_list, middle)) != NULL)
 		{
 			free(*word);
 			(*word) = tmpstart;
 			(*word) = ft_strlink(word, var);
+			(*word) = ft_strlink(word, tmpend);
 			free(tmpend);
 			return (1);
 		}
@@ -80,7 +80,6 @@ int		modify_word(char **word, t_env_list *env_list, off_t i)
 			return (1);
 		}
 	}
-	printf("ERROR\n");
 	return (-1);
 }
 
@@ -100,7 +99,7 @@ int 	manage_money(t_env_list *env_list, char **words)
 	i = 0;
 	while (words[i])
 	{
-		if (ft_strchr(words[i], '$') != 0)
+		while (ft_strchr(words[i], '$') != 0)
 			if (start_replacement(&words[i], env_list) == -1)
 				return (-1);
 		i++;
